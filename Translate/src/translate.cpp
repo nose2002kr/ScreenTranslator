bab238420bc56ecafa8eeca0554e11a71ccf85ee
nose2002kr <nose2002kr@naver.com>
2019-01-14 20:13:39 +0900

    config Translate project

--- Translate/src/translate.cpp
+++ Translate/src/translate.cpp
@@ -1,2 +1,33 @@
+#include <stdio.h>
+#include <curl/curl.h>
 
-// https://translation.googleapis.com/language/translate/v2?q=hello&target=ko&format=text&source=en&key=AIzaSyB3MXUyZEcDiw4VQ8tucT3lcvOyr5VbIh0
+int main() {
+  CURL *curl;
+  CURLcode res;
+
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+
+  curl = curl_easy_init();
+  if (curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://translation.googleapis.com/language/translate/v2?q=hello&target=ko&format=text&source=en&key=AIzaSyB3MXUyZEcDiw4VQ8tucT3lcvOyr5VbIh0");
+
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if (res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+        curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+
+  curl_global_cleanup();
+
+
+  return 0;
+}
+// 
