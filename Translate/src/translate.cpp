a74a9f8371bcb24374eaf168baffcf1f0cdcfe7d
nose2002kr <nose2002kr@naver.com>
2019-01-17 21:26:10 +0900

    remove 64bit build config, and Translate project libcurl replaced for ssl

--- Translate/src/translate.cpp
+++ Translate/src/translate.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <memory>
 #include <string>
+
+#define CURL_STATICLIB
+#define USE_OPENSSL
 #include <curl/curl.h>
 
 size_t CurlWrite_CallbackFunc_StdString(void *contents, size_t size, size_t nmemb, std::string *s)
