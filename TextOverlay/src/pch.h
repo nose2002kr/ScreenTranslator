1623d503fa9a889207d7dc577d8e9f95e7825c8e
nose2002kr <nose2002kr@naver.com>
2020-09-20 15:26:41 +0900

    fix to by using d3d11, for better the screen capture function.

---
+++ TextOverlay/src/pch.h
@@ -0,0 +1,21 @@
+#include <exception>
+
+#include "wil/cppwinrt.h"
+
+#include <D2d1_1.h>
+#include <D3d11_4.h>
+#include <Dwrite.h>
+
+
+#include <vector>
+#include <map>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Metadata.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.Graphics.Capture.h>
+#include <winrt/Windows.Graphics.DirectX.h>
+#include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+
+#include <wincodec.h>
