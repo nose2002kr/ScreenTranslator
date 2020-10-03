c4e226b16f18b8d6a02c93b74c2d769a8ac3388a
nose2002kr <nose2002kr@naver.com>
2020-10-03 21:40:22 +0900

    increased debug function usability

---
+++ CommonUtil/src/debug_util.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define DEBUG_LEVEL3
+
+#ifdef DEBUG_LEVEL3
+#define DEBUG_LEVEL3 "lv3_"
+#define DEBUG_LEVEL2 "lv2_"
+#endif
+
+#ifdef DEBUG_LEVEL2
+#define DEBUG_LEVEL2 "lv2_"
+#define DEBUG_LEVEL1 "lv1_"
+#endif
--- OCR/src/ocr.cpp
+++ OCR/src/ocr.cpp
@@ -1,6 +1,6 @@
 #include "ocr.h"
 
-#define DEBUG_LEVEL2
+#include "debug_util.h"
 OCR* OCR::g_inst = nullptr;
 
 OCR::OCR(std::string tessdataPath) {
@@ -25,6 +25,7 @@ OCR::findOutTextInfos(Image *imgParam) {
 bool
 OCR::findOutTextInfos(cv::Mat img, int relx, int rely, bool useDiff) {
   bool found = false;
+  if (img.empty()) return found;
 
   std::unique_lock<std::mutex> lock(m_mtx, std::try_to_lock);
   if (!useDiff 
@@ -46,8 +47,8 @@ OCR::findOutTextInfos(cv::Mat img, int relx, int rely, bool useDiff) {
       it->x += relx;
       it->y += rely;
 
-#ifdef DEBUG_LEVEL2
-      cv::imwrite("./letterBox.png", cropImg);
+#ifdef DEBUG_LEVEL1
+      cv::imwrite(DEBUG_LEVEL1"letterBox.png", cropImg);
 #endif
       api->SetImage(cropImg.data, cropImg.cols, cropImg.rows, cropImg.channels(), cropImg.step1());
       char* textOutput = api->GetUTF8Text();     // Get the text 
@@ -56,9 +57,9 @@ OCR::findOutTextInfos(cv::Mat img, int relx, int rely, bool useDiff) {
         delete[] textOutput;
         continue;
       }
-#ifdef DEBUG_LEVEL2
-      std::cout << textOutput << std::endl; // Destroy used object and release memory ocr->End();
-#endif
+
+      removeIntersectRect(imageUtil::toWinRect(*it));
+
       std::vector<cv::Vec3b> vec = imageUtil::findDominantColors(cropImg, 2);
       TextInfo tInfo{ 0, };
       
@@ -71,30 +72,32 @@ OCR::findOutTextInfos(cv::Mat img, int relx, int rely, bool useDiff) {
       found = true;
     }
 
-#ifdef DEBUG_LEVEL2
+#ifdef DEBUG_LEVEL1
     cv::Mat debugImg = img.clone();
     for (int i = 0; i < g_textInfo.size(); i++) {
       cv::rectangle(debugImg, imageUtil::toCVRect(g_textInfo[i].rect), cv::Scalar(0, 255, 0, 255), 3, 8, 0);
     }
-    cv::imwrite("./searchedImage.png", debugImg);
+    cv::imwrite(DEBUG_LEVEL1"searchedImage.png", debugImg);
 #endif
   } else {
-#ifdef DEBUG_LEVEL2
-    cv::imwrite("./img.png", img);
-    cv::imwrite("./lastImage.png", m_lastImage);
+#ifdef DEBUG_LEVEL1
+    cv::imwrite(DEBUG_LEVEL1"img.png", img);
+    cv::imwrite(DEBUG_LEVEL1"lastImage.png", m_lastImage);
 #endif
     std::vector<cv::Rect> diffRange = imageUtil::findDiffRange(m_lastImage, img);
     if (!diffRange.empty()) {
       for (auto rect : diffRange) {
         cv::Mat crop = img(rect);
-#ifdef DEBUG_LEVEL2
-        cv::imwrite("./crop.png", crop);
+#ifdef DEBUG_LEVEL1
+        cv::imwrite(DEBUG_LEVEL1"crop.png", crop);
 #endif
-        found = found | OCR::instnace()->findOutTextInfos(crop, relx + rect.x, rely + rect.y, false);
+        bool foundFromParticle = OCR::instnace()->findOutTextInfos(crop, relx + rect.x, rely + rect.y, false);
+        found = found | foundFromParticle;
       }
     }
   }
   if (found) {
     m_lastImage = img.clone();
   }
+  return found;
 }
