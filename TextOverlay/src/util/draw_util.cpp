626f5a8a06a6a50d8369a042b1429f15dc581dcc
nose2002kr <nose2002kr@naver.com>
2020-10-09 19:28:10 +0900

    remove RctW, RctH. use instaed RectWrapper

--- TextOverlay/src/util/draw_util.cpp
+++ TextOverlay/src/util/draw_util.cpp
@@ -5,19 +5,20 @@
 #include "rect_util.h"
 
 void drawDebugLine(ID2D1RenderTarget* pTarget, RECT rect) {
+  RectWrapper rectWrapper(rect);
   ID2D1SolidColorBrush* brush = nullptr;
   pTarget->CreateSolidColorBrush(D2D1::ColorF(255, 0, 0, 255), &brush);
 
   pTarget->DrawLine(D2D1::Point2<float>(0, 0), D2D1::Point2<float>(30, 0), brush, 4.0);
   pTarget->DrawLine(D2D1::Point2<float>(0, 0), D2D1::Point2<float>(0, 30), brush, 4.0);
 
-  pTarget->DrawLine(D2D1::Point2<float>((float) RctW(rect), 0), D2D1::Point2<float>((float) RctW(rect) - 30, 0), brush, 4.0);
-  pTarget->DrawLine(D2D1::Point2<float>((float) RctW(rect), 0), D2D1::Point2<float>((float) RctW(rect), 0 + 30), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>((float)rectWrapper.w(), 0), D2D1::Point2<float>((float)rectWrapper.w() - 30, 0), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>((float)rectWrapper.w(), 0), D2D1::Point2<float>((float)rectWrapper.w(), 0 + 30), brush, 4.0);
 
-  pTarget->DrawLine(D2D1::Point2<float>(0, (float) RctH(rect)), D2D1::Point2<float>(0 + 30, (float) RctH(rect)), brush, 4.0);
-  pTarget->DrawLine(D2D1::Point2<float>(0, (float) RctH(rect)), D2D1::Point2<float>(0, (float) RctH(rect) - 30), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>(0, (float)rectWrapper.h()), D2D1::Point2<float>(0 + 30, (float)rectWrapper.h()), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>(0, (float)rectWrapper.h()), D2D1::Point2<float>(0, (float)rectWrapper.h() - 30), brush, 4.0);
 
-  pTarget->DrawLine(D2D1::Point2<float>((float) RctW(rect), (float) RctH(rect)), D2D1::Point2<float>((float) RctW(rect) - 30, (float) RctH(rect)), brush, 4.0);
-  pTarget->DrawLine(D2D1::Point2<float>((float) RctW(rect), (float) RctH(rect)), D2D1::Point2<float>((float) RctW(rect), (float) RctH(rect) - 30), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>((float)rectWrapper.w(), (float)rectWrapper.h()), D2D1::Point2<float>((float)rectWrapper.w() - 30, (float)rectWrapper.h()), brush, 4.0);
+  pTarget->DrawLine(D2D1::Point2<float>((float)rectWrapper.w(), (float)rectWrapper.h()), D2D1::Point2<float>((float)rectWrapper.w(), (float)rectWrapper.h() - 30), brush, 4.0);
   brush->Release();
 }
