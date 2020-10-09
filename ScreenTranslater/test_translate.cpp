d4fcef4f877ab6762070aa361a21b28316a74875
nose2002kr <nose2002kr@naver.com>
2020-10-10 00:31:12 +0900

    pass translate parameter

--- ScreenTranslater/test_translate.cpp
+++ ScreenTranslater/test_translate.cpp
@@ -5,8 +5,8 @@
 #include "Translate/src/translate.h"
 void test::translate() {
 
-  //std::string src = "Hello world!";
-  std::string src = "HelloWorld";
+  std::string src = "Hello world!";
+  //std::string src = "HelloWorld";
   std::string translated = Translate().translate(src);
   writeLog(DEBUG, "Translate test:" + src + " - " + translated);
 }
