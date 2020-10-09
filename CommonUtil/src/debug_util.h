1ce67b950a860d6687e95da2889b6af3726e5af0
nose2002kr <nose2002kr@naver.com>
2020-10-09 21:28:36 +0900

    fix debug build error
    in progress, edit to append 'lv1_' prefix when enabled `DEBUG_LEVEL1`.

--- CommonUtil/src/debug_util.h
+++ CommonUtil/src/debug_util.h
@@ -1,6 +1,6 @@
 #pragma once
 
-//#define DEBUG_LEVEL1
+#define DEBUG_LEVEL1
 
 #ifdef DEBUG_LEVEL3
 #define DEBUG_LEVEL3 "lv3_"
@@ -9,5 +9,9 @@
 
 #ifdef DEBUG_LEVEL2
 #define DEBUG_LEVEL2 "lv2_"
+#define DEBUG_LEVEL1 "lv1_"
+#endif
+
+#ifdef DEBUG_LEVEL1
 #define DEBUG_LEVEL1 "lv1_"
 #endif
